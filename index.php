1f385a2b9dd8eb984fb763685deac1cb2f9df870
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2021-05-03 16:12:51 +0200

    addes sass for better developing

--- index.php
+++ index.php
@@ -4,13 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style/reset.css"> 
-    <link rel="stylesheet" href="style/style.css">                  
+    <link rel="stylesheet" href="dist/reset.css"> 
+    <link rel="stylesheet" href="dist/app.css">                  
     <title>Home</title>
 </head>
 <body>
 
+
+
 <h1>replastify</h1>
+
+
     
 </body>
 </html>
