aa9ba0a42ab2538818b2b0e37614916f216e4afa
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2021-05-13 11:05:03 +0200

    css for header

---
+++ header.inc.php
@@ -0,0 +1,45 @@
+
+<header>
+    <nav>
+
+        <div class="nav__logo">
+            <img class="nav__image" src="images/logo_header.png" alt="">
+        </div>
+
+            <!-- <input type="checkbox" id="hamburger" >
+            <label for="hamburger" class="hamburger" >
+            <img class="img_hamburger" src="images/hamburger.svg" alt="hamburger">
+            </label> -->
+
+
+        
+
+
+
+        <!-- <div class="menu_mob" >
+            
+                <a id="link" href="index.php#diensten_mob">Diensten</a>
+                <a id="link" href="prijzen.php">Prijzen</a>
+                <a id="link" href="offerte.php">Offerte</a>
+                <a id="link" href="contact.php">Contact</a>
+            
+        </div> -->
+
+
+        <div class="nav__menu" >
+            
+                <a class="menu__link"  href="">Home</a>
+                <a class="menu__link"  href="">3D modellen</a>
+                <a class="menu__link"  href="">Filament</a>
+                <a class="menu__link"  href="">Community</a>
+                <a class="menu__link"  href="">Verkoperspaneel</a>
+                <a class="menu__link"  href="">Profiel</a>
+            
+        </div>
+
+        
+
+        
+    </nav>
+</header>
+    
--- index.php
+++ index.php
@@ -5,15 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="dist/reset.css"> 
-    <link rel="stylesheet" href="dist/app.css">                  
-    <title>Home</title>
+    <link rel="stylesheet" href="dist/app.css">  
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&display=swap" rel="stylesheet">                
+    <title>Replastify</title>
 </head>
 <body>
 
-<header>
-
-
-</header>
+<?php include_once("header.inc.php"); ?>
 
 <h1>replastify</h1>
 
