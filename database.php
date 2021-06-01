ef8ff996ed81f5eb6f337b21b1a2d1c88e0aaaf8
jonathanverhaegen <jonathan_verhaegen@hotmail.com>
2021-06-01 10:12:34 +0200

    database page

---
+++ database.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="dist/app.css">  
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">  
+    <title>Replastify</title>
+</head>
+<body>
+
+<?php include_once("header.inc.php"); ?>
+
+<div class="container">
+
+
+<div class="searchbar">
+
+    <form class="searchbar__form form--search" action="" method="post">
+        <input type="text" class="form__input form__input--search" name="search" > 
+        <input class="btn" type="submit" value="Zoek">
+    </form>
+
+    <a class="searchbar__link" href="">Model uploaden</a>
+</div>
+
+<div class="database">
+
+    <div class="model">
+        <p class="model__name">Naam object</p>
+        <img class="model__img" src="images/skull.jpg" alt="3dmodel">
+        <div class="model__link">
+            <a class="btn" href="">Bekijk</a>
+        </div>
+        
+    </div>
+
+    <div class="model">
+        <p class="model__name">Naam object</p>
+        <img class="model__img" src="images/skull.jpg" alt="3dmodel">
+        <div class="model__link">
+            <a class="btn" href="">Bekijk</a>
+        </div>
+        
+    </div>
+
+    <div class="model">
+        <p class="model__name">Naam object</p>
+        <img class="model__img" src="images/skull.jpg" alt="3dmodel">
+        <div class="model__link">
+            <a class="btn" href="">Bekijk</a>
+        </div>
+        
+    </div>
+
+    <div class="model">
+        <p class="model__name">Naam object</p>
+        <img class="model__img" src="images/skull.jpg" alt="3dmodel">
+        <div class="model__link">
+            <a class="btn" href="">Bekijk</a>
+        </div>
+        
+    </div>
+
+    <div class="model">
+        <p class="model__name">Naam object</p>
+        <img class="model__img" src="images/skull.jpg" alt="3dmodel">
+        <div class="model__link">
+            <a class="btn" href="">Bekijk</a>
+        </div>
+        
+    </div>
+
+    <div class="model">
+        <p class="model__name">Naam object</p>
+        <img class="model__img" src="images/skull.jpg" alt="3dmodel">
+        <div class="model__link">
+            <a class="btn" href="">Bekijk</a>
+        </div>
+        
+    </div>
+
+    <div class="model">
+        <p class="model__name">Naam object</p>
+        <img class="model__img" src="images/skull.jpg" alt="3dmodel">
+        <div class="model__link">
+            <a class="btn" href="">Bekijk</a>
+        </div>
+        
+    </div>
+
+    <div class="model">
+        <p class="model__name">Naam object</p>
+        <img class="model__img" src="images/skull.jpg" alt="3dmodel">
+        <div class="model__link">
+            <a class="btn" href="">Bekijk</a>
+        </div>
+        
+    </div>
+
+</div>
+
+
+
+</div>
+<?php include_once("footer.inc.php") ?>
+    
+</body>
+</html>
